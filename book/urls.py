5e6ae4e95e5934be742198e8a54099621ea7139a
jgrynczewski <Tel^)%322))$>
2021-08-21 10:34:07 +0200

    Widok detalu

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,6 @@ from book import views
 app_name = 'book'
 
 urlpatterns = [
-    path('list/', views.BookListView.as_view(), name='book-list')
+    path('list/', views.BookListView.as_view(), name='book-list'),
+    path('detail/<int:pk>', views.BookDetailView.as_view(), name='book-detail')
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from book.models import Book
 
 
 class BookListView(ListView):
     model=Book
+
+
+class BookDetailView(DetailView):
+    model = Book
+
